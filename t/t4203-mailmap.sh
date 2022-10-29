8f9e6ea1f7212841a067e37aabe90dc79b724287
Siddharth Asthana <siddharthasthana31@gmail.com>
2022-10-29 15:54:58 +0530

    cat-file: add mailmap support to -s option
    Even though the cat-file command with `-s` option does not complain when
    `--use-mailmap` option is given, the latter option is ignored. Compute
    the size of the object after replacing the idents and report it instead.
    
    In order to make `-s` option honour the mailmap mechanism we have to
    read the contents of the commit/tag object. Make use of the call to
    `oid_object_info_extended()` to get the contents of the object and store
    in `buf`. `buf` is later freed in the function.
    
    Mentored-by: Christian Couder <christian.couder@gmail.com>
    Mentored-by: John Cai <johncai86@gmail.com>
    Signed-off-by: Siddharth Asthana <siddharthasthana31@gmail.com>
    Signed-off-by: Taylor Blau <me@ttaylorr.com>

--- t/t4203-mailmap.sh
+++ t/t4203-mailmap.sh
@@ -1022,4 +1022,33 @@ test_expect_success '--mailmap enables mailmap in cat-file for annotated tag obj
 	test_cmp expect actual
 '
 
+test_expect_success 'git cat-file -s returns correct size with --use-mailmap' '
+	test_when_finished "rm .mailmap" &&
+	cat >.mailmap <<-\EOF &&
+	C O Mitter <committer@example.com> Orig <orig@example.com>
+	EOF
+	cat >expect <<-\EOF &&
+	209
+	220
+	EOF
+	git cat-file -s HEAD >actual &&
+	git cat-file --use-mailmap -s HEAD >>actual &&
+	test_cmp expect actual
+'
+
+test_expect_success 'git cat-file -s returns correct size with --use-mailmap for tag objects' '
+	test_when_finished "rm .mailmap" &&
+	cat >.mailmap <<-\EOF &&
+	Orig <orig@example.com> C O Mitter <committer@example.com>
+	EOF
+	git tag -a -m "annotated tag" v3 &&
+	cat >expect <<-\EOF &&
+	141
+	130
+	EOF
+	git cat-file -s v3 >actual &&
+	git cat-file --use-mailmap -s v3 >>actual &&
+	test_cmp expect actual
+'
+
 test_done
